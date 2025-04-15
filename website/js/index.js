4061ac6ae1985eb03ba64d80d71d7b8ea4f5b984
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-15 21:46:08 +0900

    Finished section 11.

--- website/js/index.js
+++ website/js/index.js
@@ -1,23 +1,27 @@
 //
 
-const myText = document.getElementById('myText');
+const myCheckBox = document.getElementById('myCheckBox');
+const visaBtn = document.getElementById('visaBtn');
+const masterCardBtn = document.getElementById('masterCardBtn');
+const payPalBtn = document.getElementById('payPalBtn');
 const mySubmit = document.getElementById('mySubmit');
-const resultElement = document.getElementById('resultElement');
-let age;
+const subResult = document.getElementById('subResult');
+const paymentResult = document.getElementById('paymentResult');
 
 mySubmit.onclick = function() {
-
-   age = Number(myText.value);
-
-   if (age >= 100) {
-      resultElement.textContent = `You are TOO OLD to enter this site!`;
-   } else if (age == 0) {
-      resultElement.textContent = `You can't enter. You were just born!`;
-   } else if (age > 18) {
-      resultElement.textContent = `You are old enough to enter this site.`;
-   } else if (age < 0) {
-      resultElement.textContent = `Your age can't be below 0.`;
+   if (myCheckBox.checked) {
+      subResult.textContent = `You are subscribed!`;
    } else {
-      resultElement.textContent = `You must be 18+ to enter thid site!`;
+      subResult.textContent = `You are NOT subscribed`;
+   }
+
+   if (visaBtn.checked) {
+      paymentResult.textContent = `You are paying with Visa`;
+   } else if (masterCardBtn.checked) {
+      paymentResult.textContent = `You are paying with MasterCard`;
+   } else if (payPalBtn.checked) {
+      paymentResult.textContent = `You are paying with PayPal`;
+   } else {
+      paymentResult.textContent = `You must select a payment type`;
    }
 }
