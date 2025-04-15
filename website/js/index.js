54454a126fca47d237b375a4674ac5cc72f58c94
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-15 18:55:02 +0900

    Finished section 6.

--- website/js/index.js
+++ website/js/index.js
@@ -1,60 +1,12 @@
-// let age = window.prompt("How old are you?");
+const PI = 3.14159;
+let radius;
+let circumference;
 
-// age = Number(age);
-// age += 1;
+document.getElementById("mySubmit").onclick = function() {
+   radius = document.getElementById("myText").value;
+   radius = Number(radius);
+   
+   circumference = 2 * PI * radius;
 
-// console.log(age, typeof age);
-
-let x;
-let y;
-let z;
-
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
-
-console.log("---------");
-
-x = 'pizza';
-y = 'pizza';
-z = 'pizza';
-
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
-
-console.log('---------');
-
-x = '0';
-y = '0';
-z = '0';
-
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
-
-console.log('---------');
-
-x = '';
-y = '';
-z = '';
-
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
+   document.getElementById('myH3').textContent = circumference + "cm";
+}
