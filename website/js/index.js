4e7fba6afc2dd4d936db77d15407d1ad4caae604
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-15 18:33:45 +0900

    Finished section 5.

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,60 @@
-let username;
+// let age = window.prompt("How old are you?");
 
-document.getElementById('mySubmit').onclick = function() {
-  username = document.getElementById('myText').value;
-  document.getElementById('myH1').textContent = `Hello ${username}`;
-};
+// age = Number(age);
+// age += 1;
+
+// console.log(age, typeof age);
+
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+console.log("---------");
+
+x = 'pizza';
+y = 'pizza';
+z = 'pizza';
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+console.log('---------');
+
+x = '0';
+y = '0';
+z = '0';
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+console.log('---------');
+
+x = '';
+y = '';
+z = '';
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
