ee873fe0900e037aaa525ed341b1fedfe6eac440
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-15 19:32:33 +0900

    Finished section 8.

--- website/js/index.js
+++ website/js/index.js
@@ -1,20 +1,26 @@
-const decreareBtn = document.getElementById('decreaseBtn');
-const resetBtn = document.getElementById('resetBtn');
-const increaseBtn = document.getElementById('increaseBtn');
-const countLabel = document.getElementById('countLabel');
-let count = 0;
+//console.log(Math.PI);
+//console.log(Math.E);
 
-increaseBtn.onclick = function() {
-   count++;
-   countLabel.textContent = count;
-}
+let x = 3;
+let y = 2;
+let z = 1;
 
-decreaseBtn.onclick = function () {
-   count--;
-   countLabel.textContent = count;
-};
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(x, y);
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.cos(x);
+//z = Math.tan(x);
+//z = Math.abs(x);
+z = Math.sign(x);
 
-resetBtn.onclick = function () {
-  count = 0;
-  countLabel.textContent = count;
-};
+console.log(z);
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(min);
