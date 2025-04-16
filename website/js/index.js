26a7e8b21834c7654415cc63210ff6edcdc89190
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-16 12:37:12 +0900

    Finiahed section 15.

--- website/js/index.js
+++ website/js/index.js
@@ -1,21 +1,31 @@
 //
 
-let username = "BroCode";
+//const fullName = "Bro Code";
 
-console.log(username.charAt(0));
-console.log(username.indexOf("o"));
-console.log(username.lastIndexOf('o'));
-console.log(username.length);
-console.log(username.trim());
-console.log(username.toUpperCase());
-console.log(username.toLocaleLowerCase());
-console.log(username.repeat(3));
-console.log(username.startsWith(' '));
-console.log(username.endsWith(' '));
-console.log(username.includes(' '));
+// let firstName = fullName.slice(0, 3);
+// let lastName = fullName.slice(4);
 
-let phoneNumber = "123-456-7890";
+// console.log(firstName);
+// console.log(lastName);
 
-console.log(phoneNumber.replaceAll('-', '/'));
-console.log(phoneNumber.padStart(15, '0'));
-console.log(phoneNumber.padEnd(15, '0'));
+// let firstChar = fullName.slice(0, 1);
+// let lastChar = fullName.slice(-1);
+
+// console.log(firstChar);
+// console.log(lastChar);
+
+const fullName = "Broseph Code";
+
+let firstName = fullName.slice(0, fullName.indexOf(' '));
+let lastName = fullName.slice(fullName.indexOf(' ') + 1);
+
+console.log(firstName);
+console.log(lastName);
+
+const email = "Bro1@gmail.com";
+
+let username = email.slice(0, email.indexOf('@'));
+let extension = email.slice(email.indexOf('@') + 1);
+
+console.log(username)
+console.log(extension);
