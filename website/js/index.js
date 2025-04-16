f22b82ab9a5e76409c0d3bb7bcdca5a546bf252f
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-16 13:25:04 +0900

    Finished section 18.

--- website/js/index.js
+++ website/js/index.js
@@ -1,21 +1,9 @@
 //
 
-// ----- NO METHOD CHAINING -----
+const PI = 3.14;
 
-let username = window.prompt("Enter your username: ");
-
-// username = username.trim();
-// let letter = username.charAt(0);
-// letter = letter.toUpperCase();
-
-// let extraChars = username.slice(1);
-// extraChars = extraChars.toLocaleLowerCase();
-// username = letter + extraChars;
-
-console.log(username);
-
-// ---- METHOD CHAINING -----
-
-username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
-
-console.log(username);
+if (PI === "3.14") {
+   console.log("That is Pi");
+} else {
+   console.log("That is NOT Pi")
+}
