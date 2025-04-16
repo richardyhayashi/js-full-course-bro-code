d485fa787773fe342dbe684a8c258f73cf86676b
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-16 15:06:55 +0900

    Finished section 21; number guessing game.

--- website/js/index.js
+++ website/js/index.js
@@ -1,9 +1,32 @@
-//
+// NUMBER GUESSING GAME
 
-const PI = 3.14;
+const MINIMUM = 1;
+const MAXIMUM = 100;
+const answer = Math.floor(Math.random() * (MAXIMUM - MINIMUM + 1)) + MINIMUM;
 
-if (PI === "3.14") {
-   console.log("That is Pi");
-} else {
-   console.log("That is NOT Pi")
+let attempts = 0;
+let running = true;
+let guess;
+
+while (running) {
+   guess = window.prompt(`Guess a number between ${MINIMUM} - ${MAXIMUM}`);
+   guess = Number(guess);
+
+   if (isNaN(guess)) {
+      window.alert("Please enter a valid number");
+      continue;
+   } else if (guess < MINIMUM || guess > MAXIMUM) {
+      window.alert('Please enter a valid number');
+      continue;
+   }
+
+   attempts++;
+   if (guess < answer) {
+      window.alert('TOO LOW! TRY AGAIN!');
+   } else if (guess > answer) {
+      window.alert('TOO HIGH! TRY AGAIN!');
+   } else {
+      window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
+      running = false;
+   }
 }
