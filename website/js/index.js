70a571bd1b43d6fa2bd42691bbf922fc920d65c6
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-16 18:03:56 +0900

    Finished section 28; Dice Roller.

--- website/js/index.js
+++ website/js/index.js
@@ -1,61 +1,18 @@
-// rest parameters
+// DICE ROLLER PROGRAM
 
-// function openFridge(...foods) {
-//    console.log(...foods);
-// }
+function rollDice() {
+   const numOfDice = document.getElementById("numOfDice").value;
+   const diceResult = document.getElementById('diceResult');
+   const diceImages = document.getElementById('diceImages');
+   const values = [];
+   const images = [];
 
-// function getFood(...foods) {
-//    return foods;
-// }
+   for(let i = 0; i < numOfDice; i++) {
+      const value = Math.floor(Math.random() * 6) + 1;
+      values.push(value);
+      images.push(`<img src="./img/dice_imgs/${value}.png" alt=${value} />`);
+   }
 
-// const food1 = 'pizza';
-// const food2 = 'hamburger';
-// const food3 = 'hotdog';
-// const food4 = 'sushi';
-// const food5 = 'ramen';
-
-// openFridge(food1, food2, food3, food4, food5);
-
-// const foods = getFood(food1, food2, food3, food4, food5);
-
-// console.log(foods);
-
-
-
-// function sum(...numbers) {
-//    let result = 0;
-
-//    for (let number of numbers) {
-//       result += number;
-//    }
-
-//    return result;
-// }
-
-// function getAverage(...numbers) {
-//   let result = 0;
-
-//   for (let number of numbers) {
-//     result += number;
-//   }
-
-//   return result / numbers.length;
-// }
-
-// const total = sum(1, 2, 3, 4, 5);
-
-// console.log(`Your total is $${total}`);
-
-// const avg = getAverage(75, 100, 85, 90, 50);
-
-// console.log(avg);
-
-
-
-function combineStrings(...strings) {
-   return strings.join(" ");
+   diceResult.textContent = `dice: ${values.join(', ')}`;
+   diceImages.innerHTML = images.join('');
 }
-
-const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III");
-
-console.log(fullName);
