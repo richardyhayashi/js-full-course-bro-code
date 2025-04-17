1c19a1430670f0de40a1c73a5d6cc813cb59fb28
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-17 21:26:19 +0900

    Finished section 46.

--- website/js/index.js
+++ website/js/index.js
@@ -1,72 +1,58 @@
-// destructuring
+// nested objects
 
-// --- EXAMPLE 1 ---
-// SWAP THE VALUE OF TWO VARIABLES
+// const person = {
+//   fullName: "Spongebob Squarepants",
+//   age: 30,
+//   isStudent: true,
+//   hobbies: ["karate", "jellyfishing", "cooking"],
+//   address: {
+//     street: "124 Conch St.",
+//     city: "Bikini Bottom",
+//     country: "Intl. Waters"
+//   }
+// }
 
-let a = 1;
-let b = 2;
+// console.log(person.fullName);
+// console.log(person.age);
+// console.log(person.isStudent);
+// console.log(person.hobbies[2]);
+// console.log(person.address.country);
 
-[a, b] = [b, a]
-
-console.log(a);
-console.log(b);
-
-
-// --- EXAMPLE 2 ---
-// SWAP 2 ELEMENTS IN AN ARRAY
-
-const colors = ["red", "green", "blue", "black", "white"];
-
-[colors[0], colors[4]] = [colors[4], colors[0]];
-
-console.log(colors);
-
-
-// --- EXAMPLE 3 ---
-// ASSIGN ARRAY ELEMENTS TO VARIABLES
-
-const [firstColor, secondColor, thirdColor, ...extraColors] = colors;
-
-console.log(firstColor);
-console.log(secondColor);
-console.log(thirdColor);
-console.log(extraColors);
+// for (const property in person.address) {
+//   console.log(person.address[property]);
+// }
 
 
-// --- EXAMPLE 4 ---
-// EXTRACT VALUES FROM OBJECTS
 
-const person1 = {
-  firstName: "Spongebob",
-  lastName: "Squarepants",
-  age: 30,
-  job: "Fry Cook",
+class Person {
+  constructor(name, age, ...address) {
+    this.name = name;
+    this.age = age;
+    this.address = new Address(...address);
+  }
 }
 
-const person2 = {
-  firstName: 'Patrick',
-  lastName: 'Star',
-  age: 34,
-};
-
-
-//const {firstName, lastName, age, job} = person1;
-const { firstName, lastName, age, job='Unemployed' } = person2;
-
-console.log(firstName);
-console.log(lastName);
-console.log(age);
-console.log(job);
-
-
-// --- EXAMPLE 5 ---
-// DESTRUCTURE IN FUNCTION PARAMETERS
-
-function displayPerson({firstName, lastName, age, job="Unemployed"}) {
-  console.log(`name: ${firstName} ${lastName}`);
-  console.log(`age: ${age}`);
-  console.log(`job: ${job}`);
+class Address {
+    constructor(street, city, country) {
+      this.street = street;
+      this.city = city;
+      this.country =country;
+    }
 }
 
-displayPerson(person1);
-displayPerson(person2);
+const person1 = new Person("Spongebob", 30, "124 Conch St.",
+                                            "Bikini Bottom",
+                                            "Intl. Waters");
+const person2 = new Person('Patrick', 37, '128 Conch St.',
+                                          'Bikini Bottom',
+                                          'Intl. Waters');
+const person3 = new Person('Squidward', 45, '126 Conch St.',
+                                            'Bikini Bottom',
+                                            'Intl. Waters');
+
+console.log(person3.name);
+console.log(person3.age);
+console.log(person3.address);
+console.log(person3.address.street);
+console.log(person3.address.city);
+console.log(person3.address.country);
