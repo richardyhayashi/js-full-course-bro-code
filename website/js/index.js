666291aa1bdf2c4c8493d6bf63de713c31e02559
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-17 20:10:22 +0900

    Finished section 44.

--- website/js/index.js
+++ website/js/index.js
@@ -1,70 +1,102 @@
-// super
+// getters & setters
 
-class Animal {
-  constructor(name, age) {
-      this.name = name;
-      this.age = age;
+// class Rectangle {
+//   constructor(width, height) {
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   set width(newWidth) {
+//     if (newWidth > 0) {
+//       this._width = newWidth;
+//     } else {
+//       console.error('width must be a positive number');
+//     }
+//   }
+
+//   get width() {
+//     return `${this._width.toFixed(1)}cm`;
+//   }
+
+//   set height(newHeight) {
+//     if (newHeight > 0) {
+//       this._height = newHeight;
+//     } else {
+//       console.error('height must be a positive number');
+//     }
+//   }
+
+//   get height() {
+//     return `${this._height.toFixed(1)}cm`;
+//   }
+
+//   get area() {
+//     return `${(this._width * this._height).toFixed(1)}cm^2`;
+//   }
+// }
+
+// const rectangle = new Rectangle(3, 4);
+
+// rectangle.width = 5;
+// rectangle.height = 6;
+
+// console.log(rectangle.width);
+// console.log(rectangle.height);
+// console.log(rectangle.area);
+
+
+
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
   }
 
-  move(speed) {
-   console.log(`The ${this.name} moves at a speed of ${speed}mph`);
+  set firstName(newFirstName) {
+    if (typeof newFirstName === 'string' && newFirstName.length > 0) {
+      this._firstName = newFirstName;
+    } else {
+      console.error('First name must be a non-empty string!');
+    }
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  set lastName(newLastName) {
+    if (typeof newLastName === 'string' && newLastName.length > 0) {
+      this._lastName = newLastName;
+    } else {
+      console.error('Last name must be a non-empty string!');
+    }
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  set age(newAge) {
+    if (typeof newAge === 'number' && newAge >= 0) {
+      this._age = newAge;
+    } else {
+      console.error('Age must be a non-negative number');
+    }
+  }
+
+  get age() {
+    return this._age;
   }
 }
 
-class Rabbit extends Animal {
-  constructor(name, age, runSpeed) {
-    super(name, age);
+const person = new Person("Spongebob", "Squarepants", 30);
 
-    this.runSpeed = runSpeed;
-  }
-
-  run() {
-   console.log(`This ${this.name} can run`);
-   super.move(this.runSpeed);
-  }
-}
-
-class Fish extends Animal {
-  constructor(name, age, swimSpeed) {
-    super(name, age);
-
-    this.swimSpeed = swimSpeed;
-  }
-
-  swim() {
-    console.log(`This ${this.name} can swim`);
-    super.move(this.swimSpeed);
-  }
-}
-
-class Hawk extends Animal {
-  constructor(name, age, flySpeed) {
-    super(name, age);
-
-    this.flySpeed = flySpeed;
-  }
-
-  fly() {
-    console.log(`This ${this.name} can fly`);
-    super.move(this.flySpeed);
-  }
-}
-
-const rabbit = new Rabbit('rabbit', 1, 25);
-const fish = new Fish('fish', 2, 12);
-const hawk = new Hawk('hawk', 3, 50);
-
-console.log(rabbit.name);
-console.log(rabbit.age);
-console.log(rabbit.runSpeed);
-rabbit.run();
-
-console.log(fish.name);
-console.log(fish.age);
-console.log(fish.swimSpeed);
-fish.swim();
-
-console.log(hawk.name);
-console.log(hawk.age);
-console.log(hawk.flySpeed);
-hawk.fly();
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+console.log(person.age);
