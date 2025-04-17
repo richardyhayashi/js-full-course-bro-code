24d7a55ac2b9ede662bbefc72e218f2a17f7e6ed
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-17 16:46:30 +0900

    Finished section 38.

--- website/js/index.js
+++ website/js/index.js
@@ -1,33 +1,28 @@
-// object
+// this
 
-const person1 = {
-   firstName: "Spongebob",
-   lastName: "Squaerpants",
-   age: 30,
-   isEmployed: true,
-   sayHello: () => {console.log("Hi! I am Spongebob!")},
-   eat: () => {console.log("I am eating a Krabby Patty")},
-};
+// const person1 = {
+//    name: "Spongbob",
+//    favFood: "hamburgers",
+//    sayHello: function() {console.log(`Hi I am ${this.name}`)},
+//    eat: function() {console.log(`${this.name} is eating ${this.favFood}`)}
+// }
+// const person2 = {
+//   name: 'Patrick',
+//   favFood: 'Pizza',
+//   sayHello: function () {
+//     console.log(`Hi I am ${this.name}`);
+//   },
+//   eat: function () {
+//     console.log(`${this.name} is eating ${this.favFood}`);
+//   },
+// };
 
-const person2 = {
-  firstName: 'Patrick',
-  lastName: 'Star',
-  age: 42,
-  isEmployed: false,
-  sayHello: () => {console.log("Hey, I'm Patrick...")},
-  eat: () => {console.log("I am eating roast beef, chicken, an pizza");}
-};
+// person1.sayHello();
+// person1.eat();
 
-console.log(person1.firstName);
-console.log(person1.lastName);
-console.log(person1.age);
-console.log(person1.isEmployed);
-person1.sayHello();
-person1.eat();
+// person2.sayHello();
+// person2.eat();
 
-console.log(person2.firstName);
-console.log(person2.lastName);
-console.log(person2.age);
-console.log(person2.isEmployed);
-person2.sayHello();
-person2.eat();
+
+
+console.log(this)
