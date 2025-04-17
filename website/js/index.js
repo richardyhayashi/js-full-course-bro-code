d8574356bdc5ed894c800e26ead54a8324665fbc
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-17 17:47:41 +0900

    Finished section 41.

--- website/js/index.js
+++ website/js/index.js
@@ -1,34 +1,54 @@
-// class
+// static
 
-class Product {
-   constructor(name, price) {
-      this.name = name;
-      this.price = price;
+// class MathUtil {
+//    static PI = 3.14159;
+
+//    static getDiameter(radius) {
+//       return 2 * radius;
+//    }
+//    static getCircumference(radius) {
+//       return 2 * this.PI * radius;
+//    }
+//    static getArea(radius) {
+//       return this.PI * Math.pow(radius, 2);
+//    }
+// }
+
+// console.log(MathUtil.PI);
+// console.log(MathUtil.getDiameter(10));
+// console.log(MathUtil.getCircumference(10));
+// console.log(MathUtil.getArea(10));
+
+
+
+class User {
+   static userCount = 0;
+
+   constructor(username) {
+      this.username = username;
+
+      User.userCount++;
    }
 
-   displayProduct() {
-      console.log(`Product: ${this.name}`);
-      console.log(`Price: $${this.price.toFixed(2)}`);
+   static getUserCount() {
+      console.log(`There are ${User.userCount} users online`);
    }
 
-   calculateTotal(salesTax) {
-      return this.price + (this.price * salesTax)
+   sayHello() {
+      console.log(`Hello, my username is ${this.username}`)
    }
 }
-const salesTax = 0.05;
 
-const product1 = new Product('Shirt', 19.99);
-const product2 = new Product('Pants', 22.50);
-const product3 = new Product('Underware', 100.00);
+const user1 = new User('Spongebob');
+const user2 = new User('Patrick');
+const user3 = new User('Sandy');
 
-product1.displayProduct();
-let total = product1.calculateTotal(salesTax);
-console.log(`Total price (with tax): $${total.toFixed(2)}`);
+//console.log(user1.username);
+//console.log(user2.username);
+//console.log(user3.username);
 
-product2.displayProduct();
-total = product2.calculateTotal(salesTax);
-console.log(`Total price (with tax): $${total.toFixed(2)}`);
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
 
-product3.displayProduct();
-total = product3.calculateTotal(salesTax);
-console.log(`Total price (with tax): $${total.toFixed(2)}`);
+User.getUserCount();
