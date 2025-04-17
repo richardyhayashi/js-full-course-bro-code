3aaee3345bbc1ee95597054b3c2b19535bcbe17a
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-17 16:34:38 +0900

    Finished section 37.

--- website/js/index.js
+++ website/js/index.js
@@ -1,26 +1,33 @@
-// arrow functions
+// object
 
-// const hello = (name, age) => {console.log(`Hello ${name}`)
-//                               console.log(`You are ${age} years old`)};
+const person1 = {
+   firstName: "Spongebob",
+   lastName: "Squaerpants",
+   age: 30,
+   isEmployed: true,
+   sayHello: () => {console.log("Hi! I am Spongebob!")},
+   eat: () => {console.log("I am eating a Krabby Patty")},
+};
 
-// hello("Richard", 25);
+const person2 = {
+  firstName: 'Patrick',
+  lastName: 'Star',
+  age: 42,
+  isEmployed: false,
+  sayHello: () => {console.log("Hey, I'm Patrick...")},
+  eat: () => {console.log("I am eating roast beef, chicken, an pizza");}
+};
 
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.isEmployed);
+person1.sayHello();
+person1.eat();
 
-
-//setTimeout(() => console.log("Hello"), 3000);
-
-
-
-const numbers = [1, 2, 3, 4, 5, 6];
-
-const squares = numbers.map((element) => Math.pow(element, 2));
-const cubes = numbers.map((element) => Math.pow(element, 3));
-const evenNums = numbers.filter((element) => element % 2 === 0);
-const oddNums = numbers.filter((element) => element % 2 !== 0);
-const total = numbers.reduce((accumulator, element) => accumulator + element);
-
-console.log(squares);
-console.log(cubes);
-console.log(evenNums);
-console.log(oddNums);
-console.log(total);
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
+person2.sayHello();
+person2.eat();
