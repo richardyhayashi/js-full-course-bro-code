95649bc8588765d4aaae22c25f784502a5f03f3d
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-18 14:26:55 +0900

    Finished section 51.

--- website/js/index.js
+++ website/js/index.js
@@ -1,52 +1,67 @@
-// Date objects
+// closure
 
-// let date = new Date();
+// function outer() {
+//   let message = 'Hello';
 
-// console.log(date);
+//   function inner() {
+//     console.log(message);
+//   }
 
+//   inner();
+// }
 
-
-// Date(year, month, day, hour, minute, second, ms)
-//date = new Date(2024, 0, 1, 2, 3, 4, 5);
-//date = new Date('2024-01-02T12:00:00Z');
-// date = new Date(0);
-
-// console.log(date);
-
-
-// const date = new Date();
-
-// console.log(date.getFullYear());
-// // Month: 0 - Jan, 1 - Feb, ..., 11 - Dec
-// console.log(date.getMonth());
-// // Date: 1 - 31
-// console.log(date.getDate());
-// // Hours: 0 - 23
-// console.log(date.getHours());
-// // Minutes: 0 -59
-// console.log(date.getMinutes());
-// // Seconds: 0 - 59
-// console.log(date.getSeconds());
-// // 0 - Sunday, 1 - Monday, ..., 6 - Saturday
-// console.log(date.getDay());
+// outer();
 
 
 
-// date.setFullYear(2024);
-// date.setMonth(0);
-// date.setDate(1);
-// date.setHours(2);
-// date.setMinutes(3);
-// date.setSeconds(4);
+// function createCounter() {
+//   let count = 0;
 
-// console.log(date);
+//   function increment() {
+//     count++;
+//     console.log(`Count increment to ${count}`);
+//   }
+
+//   function getCount() {
+//     return count;
+//   }
+
+//   return {increment, getCount};
+// }
+
+// const counter = createCounter();
+// counter.increment();
+// counter.increment();
+// counter.increment();
+// counter.increment();
+
+// console.log(`The current count is ${counter.getCount()}`);
 
 
 
-const date1 = new Date('2023-12-31');
-//const date2 = new Date('2024-01-01');
-const date2 = new Date('2023-12-30');
+function createGame() {
+  let score = 0;
 
-if (date2 > date1) {
-  console.log("Happy New Year!");
+  function increaseScore(points) {
+    score += points;
+    console.log(`+${points}pts`);
+  }
+
+  function decreaseScore(points) {
+    score -= points;
+    console.log(`-${points}pts`);
+  }
+
+  function getScore() {
+    return score;
+  }
+
+  return {increaseScore, decreaseScore, getScore};
 }
+
+const game = createGame();
+
+game.increaseScore(5);
+game.increaseScore(6);
+game.decreaseScore(3);
+console.log(`The final score is ${game.getScore()}`);
