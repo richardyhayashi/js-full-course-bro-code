4df3471a9d42fde9e0e7d94767c141bcbae41203
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-18 16:03:29 +0900

    Finished section 57.

--- website/js/index.js
+++ website/js/index.js
@@ -1,16 +1,36 @@
-// asynchronous
+// Error
 
-function func1(callback) {
-   setTimeout(() => {
-      console.log("Task 1");
-      callback();
-   }, 3000);
+// try {
+//    console.log('Hello');
+//    // > NETWORK ERRORS
+//    // > PROMISE REJECTION
+//    // > SECURITY ERRORS
+// } catch (error) {
+//    console.error(error);
+// } finally {
+//    // > CLOSE FILES
+//    // > CLOSE CONNECTIONS
+//    // > RELEASE RESOURCES
+//    console.log('This always executes');
+// }
+// console.log('You have reached the end!');
+
+
+
+try {
+   const dividend = Number(window.prompt("Enter a dividend: "));
+   const divisor = Number(window.prompt('Enter a divisor: '));
+
+   if (divisor == 0) {
+      throw new Error("You can't divide by zero!");
+   }
+   if (isNaN(dividend) || isNaN(divisor)) {
+      throw new Error("Values must be a number");
+   }
+
+   const result = dividend /divisor;
+   console.log(result);
+} catch (error) {
+   console.error(error);
 }
-
-function func2() {
-   console.log('Task 2');
-   console.log('Task 3');
-   console.log('Task 4');
-}
-
-func1(func2);
+console.log('You have reached the end!');
