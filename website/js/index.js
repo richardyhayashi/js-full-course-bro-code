d04ce97e0df747cdc74af9984da1f3f45d94263f
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-04-18 12:45:14 +0900

    Finished section 49.

--- website/js/index.js
+++ website/js/index.js
@@ -1,35 +1,16 @@
-// sort()
-// lexicographic = (alphabet + numbers + symbols) as strings
+// shuffle
+// Fisher-Yates algorithm
 
-//let fruits = ['apple', 'orange', 'banana', 'coconut', 'pineapple'];
+const cards = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 
-//fruits.sort();
+shuffle(cards);
 
-//console.log(fruits);
+console.log(cards);
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const random = Math.floor(Math.random() * (i + 1));
 
-
-// let numbers = [1, 10, 2, 9, 3, 8, 4, 7, 5 ,6];
-
-// numbers.sort((a, b) => a - b);
-// // Reverse
-// //numbers.sort((a, b) => b - a);
-
-// console.log(numbers);
-
-
-
-const people = [
-  { name: 'Spongebob', age: 30, gpa: 3.0 },
-  { name: 'Patrick', age: 37, gpa: 1.5 },
-  { name: 'Squidward', age: 51, gpa: 2.5 },
-  { name: 'Sandy', age: 27, gpa: 4.0 },
-]; 
-
-//people.sort((a, b) => a.age - b.age);
-//people.sort((a, b) => b.age - a.age);
-//people.sort((a, b) => a.gpa - b.gpa);
-//people.sort((a, b) => b.gpa - a.gpa);
-people.sort((a, b) => a.name.localeCompare(b.name));
-
-console.log(people);
+      [array[i], array[random]] = [array[random], array[i]];
+  }
+};
